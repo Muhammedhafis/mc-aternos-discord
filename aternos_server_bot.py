358312465250d448e043d465519d9c25f212882a
Muhammedhafis <129869634+Muhammedhafis@users.noreply.github.com>
2024-04-05 18:43:46 +0530

    Update aternos_server_bot.py

--- aternos_server_bot.py
+++ aternos_server_bot.py
@@ -3,11 +3,11 @@ import os
 from python_aternos import Client
 import time
 
-TOKEN = 'paste-your-token-here'
+TOKEN = 'MTIyNTc2OTAwMTA1Mzg0NzU3Mg.GU1bfC.zUzoNoK88GHrUczs01AuRRmeM0k3ANgWPKCQOk'
 
 client = discord.Client()
 
-aternos = Client('your-aternos-username', password='your-aternos-password')
+aternos = Client('discord_test', password='hafis123')
 
 atservers = aternos.servers
 
@@ -37,12 +37,12 @@ async def on_message(message):
         if user_message.lower() == '?server_start':
           myserv.start()
           while True:
-            ping = str(os.popen('mcstatus yourservername.aternos.me status | grep description').read())
+            ping = str(os.popen('mcstatus Note111.aternos.me status | grep description').read())
             if "offline" in ping:
               time.sleep(1)
             else:
               break
-          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||yourservername.aternos.me:serverport|| in the server address.")
+          await message.channel.send("server is now alive!!! you can join in 2-3 minutes by pasting ||Note111.aternos.me:58541|| in the server address.")
           return
 
     if message.channel.name == 'bot-cmnds':
